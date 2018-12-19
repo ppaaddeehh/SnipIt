4f27077868ba2181b111e6416c3bd6ece1b5e1db
ppaaddeehh <paddyloldev@gmail.com>
2018-12-19 12:11:52 +0000

    Initial commit

---
+++ SnipIt/SnipIt/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SnipIt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(435, 255);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseRelease);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(437, 258);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SnipIt/SnipIt/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+
+namespace SnipIt
+{
+    public partial class Form1 : Form
+    {
+        //These variables control the mouse position
+        int selectX;
+        int selectY;
+        int selectWidth;
+        int selectHeight;
+        public Pen selectPen;
+        Random rnd = new Random();
+        //This variable control when you start the right click
+        bool start = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Top = 0;
+            Left = 0;
+            Hide();
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+            using (MemoryStream s = new MemoryStream())
+            {
+                //save graphic variable into memory
+                printscreen.Save(s, ImageFormat.Bmp);
+                pictureBox1.Size = new System.Drawing.Size(Width, Height);
+                pictureBox1.Image = Image.FromStream(s);
+            }
+            Show();
+            Cursor = Cursors.Cross;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image == null)
+                return;
+            if (start)
+            {
+                pictureBox1.Refresh();
+                selectWidth = e.X - selectX;
+                selectHeight = e.Y - selectY;
+                pictureBox1.CreateGraphics().DrawRectangle(selectPen,
+                          selectX, selectY, selectWidth, selectHeight);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                selectX = e.X;
+                selectY = e.Y;
+                selectPen = new Pen(Color.Red, 1);
+                selectPen.DashStyle = DashStyle.DashDotDot;
+            }
+            pictureBox1.Refresh();
+            start = true;
+        }
+
+        private void pictureBox1_MouseRelease(object esneder, MouseEventArgs e)
+        {
+            if (pictureBox1.Image == null)
+                return;
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                pictureBox1.Refresh();
+                selectWidth = e.X - selectX;
+                selectHeight = e.Y - selectY;
+                pictureBox1.CreateGraphics().DrawRectangle(selectPen, selectX,
+                         selectY, selectWidth, selectHeight);
+
+            }
+            start = false;
+            SaveToClipboard();
+        }
+
+        private void SaveToClipboard()
+        {
+            if (selectWidth > 0)
+            {
+
+                Rectangle rect = new Rectangle(selectX, selectY, selectWidth, selectHeight);
+                Bitmap OriginalImage = new Bitmap(pictureBox1.Image, pictureBox1.Width, pictureBox1.Height);
+                Bitmap _img = new Bitmap(selectWidth, selectHeight);
+                Graphics g = Graphics.FromImage(_img);
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.DrawImage(OriginalImage, 0, 0, rect, GraphicsUnit.Pixel);
+                var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                _img.Save(@desktop + @"\SWAG.png", ImageFormat.Png);
+                Clipboard.SetImage(_img);
+            }
+            Application.Exit();
+        }
+    }
+}
